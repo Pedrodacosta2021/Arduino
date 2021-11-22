642700c747bf1ac93eb2ea28cec4ceb47bd0eb00
Pedrodacosta2021 <94700808+Pedrodacosta2021@users.noreply.github.com>
2021-11-22 16:02:36 +0100

    Create CICLE_DE_VIDA.ino

---
+++ Llenguatge de progrmació Arduino/Cicle de vida d'un programa/CICLE_DE_VIDA/CICLE_DE_VIDA.ino
@@ -0,0 +1,30 @@
+/********************************************************
+*                                                       *
+*                    TÍTOL:                             *
+*                   PLANTILA                            *
+*                                                       *
+* PEDRO DA COSTA                    DATA:19/11/2021     *
+********************/
+//******* INCLUDE ***********
+
+
+//******* VARIABLES *********
+
+
+//******* SETUP ***********
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+//******** LOOP ***********
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//******* FUNCIONS **********
+*/
+}
